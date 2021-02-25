80a797b47d7e5d521b8d4d4591b1d40e1326c24a
”Arthur <arthur.maydanuk@gmail.com>
2021-02-26 00:39:05 +0200

    feat: complete all of tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array == null) return 0;
+  let min = 0;
+  for(let i of array) {
+    if(i < min) min = i;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array == null) return 0;
+  let max = 0;
+  for(let i of array) {
+    if(i > max) max = i;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!(Array.isArray(array)) || array.length == 0) return 0;
+  let avg=0;
+  for(let i of array) {
+    avg+=i;
+  }
+  return (avg)/array.length;
 }
